17e6295f58cc7d043e7cbff752d6b3acbee697dd
Volokasse <stephen.caranton@gmail.com>
2017-06-29 15:45:24 +0200

    First commit with all the little work.
    Signed-off-by: Volokasse <stephen.caranton@gmail.com>

---
+++ README.md
@@ -0,0 +1,6 @@
+# How it works ?
+
+After global install just type : ```okgoogle "your research"```
+The result will be loaded in your favorite browser.
+
+That's all folks !
---
+++ index.js
@@ -0,0 +1,7 @@
+#! /usr/bin/env node
+
+var open       = require('open');
+var searchList = process.argv.slice(2);
+var search     = searchList.join().replace(',', ' ');
+
+open('https://www.google.fr/?gfe_rd=cr&ei=gQNVWZ-kOJfEaNPflYgI#q=' + search);
